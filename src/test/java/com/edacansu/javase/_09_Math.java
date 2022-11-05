14b414d7a6c3922367e9442cbc55b12acefa6a40
EdaCansu <edamiaj@gmail.com>
2022-11-05 13:06:17 +0300

    Math and random number

---
+++ src/test/java/com/edacansu/javase/_09_Math.java
@@ -0,0 +1,10 @@
+package com.edacansu.javase;
+
+
+public class _09_Math {
+    public static void main(String[] args) {
+        System.out.println("\"");
+        System.out.println("1.satır\n2.satır");
+        System.out.println("1.satır\n\t2.satır");
+    }
+}
---
+++ src/test/java/com/edacansu/javase/_10_Random.java
@@ -0,0 +1,18 @@
+package com.edacansu.javase;
+
+
+import java.util.Random;
+
+public class _10_Random {
+    public static void main(String[] args) {
+        System.out.println(Math.random()*4+1);
+        System.out.println( Math.floor(Math.random()*4+1));
+
+        //Random object
+        Random rastgele=new Random();
+        int number= rastgele.nextInt(4)+1;
+        // buradaki 4 bitiş noktasını gösterir ( 1 ile 4 arasındaki sayılar yani)
+        System.out.println(number);
+
+    }
+}
