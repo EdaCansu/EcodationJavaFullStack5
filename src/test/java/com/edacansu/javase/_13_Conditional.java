679af064a8378230a2f09f3085ef4a737aa3d362
EdaCansu <edamiaj@gmail.com>
2022-11-06 12:29:56 +0300

    loop and isDigit

--- src/test/java/com/edacansu/javase/_13_Conditional.java
+++ src/test/java/com/edacansu/javase/_13_Conditional.java
@@ -1,52 +1,51 @@
 package com.edacansu.javase;
 
-
 import java.util.Random;
 import java.util.Scanner;
 
+//Casting: Dönüşüm
 public class _13_Conditional {
+    //object field
+    private static int COUNTER=1;
+
     public static void main(String[] args) {
+        // kullanıcıdan aldığımız bir sayıyı tahmin eden bir oyun algoritması yazınız ?
+        // Bilgisayar rastgele 1-10 arasında bir sayı üretsin. bu üretilen sayıyı tahmin sayısı en fazla 5 olsun
 
-        public static void main (String[]args){
+        //sonsuz döngü: for(;;){}  while(true){}
+        for(;;){
+            Scanner klavye=new Scanner(System.in);
+            System.out.println("\nlütfen 1-10 arasında bir sayı giriniz");
+            char number= klavye.nextLine().charAt(0);
 
-            int number1 = 4;
-            switch (number1) {
-                case 1:
-                    System.out.println("sayı 1");
-                    break;
+            if(Character.isDigit(number)){
+                if(number<0){
+                    System.out.println("Lütfen negatif girmeyiniz");
+                }else{
+                    //Rastgele sayı
+                    Random random=new Random();
+                    int computerNumber= random.nextInt(10)+1;
+                    System.out.println(computerNumber);
 
-                case 2:
-                    System.out.println("sayı 2");
-                    break;
-
-                case 3:
-                    System.out.println("sayı 3");
-                    break;
-
-                default:
-                    System.out.println("lütfen sayı giriniz");
-                    break;
-            }
-
-
-            Scanner klavye = new Scanner(System.in);
-            System.out.println("lütfen 1-10 arasında bir sayı giriniz");
-            int number = klavye.nextInt();
-            if (number < 0) {
-                System.out.println("Lütfen negatif girmeyiniz");
-            } else {
-                //Rastgele sayı
-                Random random = new Random();
-                int computerNumber = random.nextInt(10) + 1;
-                System.out.println(computerNumber);
-
-                if (Math.abs(number) == computerNumber) {
-                    System.out.println("bilgisayar:" + computerNumber + " kişi: " + number + "Doğru tahmin ettiniz:");
-                } else if (number != computerNumber) {
-                    System.out.println("bilgisayar:" + computerNumber + " kişi: " + number + "!!!! Doğru tahmin etmediniz:");
-                } else {
-                    System.out.println("lütfen sayı giriniz");
+                    if(COUNTER>=5){
+                        System.out.println("Sayı tahmin sayınız 5 oldu ve sistemden çıkarıldınız");
+                        break;
+                    }
+                    if(Math.abs(number)==computerNumber){
+                        System.out.println("Girdiğiniz giriş sayısı: "+COUNTER);
+                        System.out.println("tahmin sayınız: "+number+ " bilgisayar sayıyı: "+computerNumber+" EŞLEŞTİ:");
+                    }else if(number!=computerNumber){
+                        System.out.println("Girdiğiniz giriş sayısı: "+COUNTER);
+                        System.out.println("tahmin sayınız: "+number+ " bilgisayar sayıyı: "+computerNumber+" EŞLEŞMEDİ:");
+                        COUNTER++;
+                    }  else{
+                        System.out.println("lütfen sayı giriniz");
+                    }
                 }
-            }
+            }else if(Character.isLetter(number))
+                System.out.println("Bu bir harftir lütfen sayı giriniz");
+            else
+                System.out.println("Bu bir özel simge  lütfen sayı giriniz");
         }
     }
+}
