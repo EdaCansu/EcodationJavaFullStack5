d86be15949c68016058e056b682b5599c67ce82d
EdaCansu <edamiaj@gmail.com>
2022-11-06 12:51:13 +0300

    formatter

---
+++ src/test/java/com/edacansu/javase/_19_Formatter.java
@@ -0,0 +1,26 @@
+package com.edacansu.javase;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class _19_Formatter {
+    public static void main(String[] args) {
+        // NOT:
+        // + sağa kaydırır
+        // - verirsek sola kaydırıyor.
+        String str="java öğreniyorum";
+        int decimal=44;
+        float point=14.53f;
+        System.out.printf("%s  %d  %f ",str,decimal,point);
+        System.out.println();
+        System.out.printf("%40s  %d  %f ",str,decimal,point);
+        System.out.println();
+        System.out.printf("%40s  %10d  %f ",str,decimal,point);
+        System.out.println();
+        System.out.printf("%40s  %10d %4.2f ",str,decimal,point);
+        System.out.println();
+        System.out.printf("%40s  %10d  %12s %4.2f ",str,decimal, "",point);
+    }
+}
