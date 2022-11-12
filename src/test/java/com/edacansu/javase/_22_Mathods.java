7431bb318a88f42e75b02892f5fa1878285f660f
EdaCansu <edamiaj@gmail.com>
2022-11-12 10:27:24 +0300

    void method

---
+++ src/test/java/com/edacansu/javase/_22_Mathods.java
@@ -0,0 +1,30 @@
+package com.edacansu.javase;
+
+public class _22_Mathods {
+
+
+    public static void voidParametresiz(){
+        System.out.println("Parametresiz void");
+    }
+
+    public static void voidParametreli(String adi, int sayi){
+        System.out.println("Parametreli void " + adi + " sayı " + sayi);
+    }
+
+    public static void main(String[] args) {
+
+        //AP =  Aynı Paket    FP = Farklı Paket
+        //AC = Aynı Class   FC = Farklı Class
+        //SC = SubClass
+
+        //                  AP  FP      AC  FC  SC
+        //public        =>  +   +       +   +   +
+        //protected     =>  +   +/-?    +   +   +
+        //private       =>  +   -       +   -   -
+        //-             =>  +   -       +   +   -
+
+        voidParametresiz();
+        voidParametreli("Eda", 29);
+
+    }
+}
