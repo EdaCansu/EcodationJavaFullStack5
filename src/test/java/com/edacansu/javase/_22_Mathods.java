b0311704c8c1d3996f38825eed5c583148e598b5
EdaCansu <edamiaj@gmail.com>
2022-11-12 11:11:14 +0300

    return method

--- src/test/java/com/edacansu/javase/_22_Mathods.java
+++ src/test/java/com/edacansu/javase/_22_Mathods.java
@@ -14,6 +14,17 @@ public class _22_Mathods {
     public static void voidParametreli(String adi){
         System.out.println("Parametreli void " + adi + "----Overloading example");
     }
+
+
+    static String returnluParametresiz(){
+        return "returnlu parametresiz";
+    }
+
+    static String returnluParametreli(String data){
+        return "returnlu parametreli " + data;
+    }
+
+
     public static void main(String[] args) {
 
         //AP =  Aynı Paket    FP = Farklı Paket
@@ -29,6 +40,12 @@ public class _22_Mathods {
         voidParametresiz();
         voidParametreli("Eda", 29);
         voidParametreli("Edoş");
+
+
+        System.out.println(returnluParametresiz());
+        String result = returnluParametresiz();
+        System.out.println(result);
+        System.out.println(returnluParametreli("Istanbul"));
 
     }
 }
